1b53ed0db6e5b3ddae64debc4cf6886e339b002c
KumarDevada <115659253+KumarDevada@users.noreply.github.com>
2024-06-09 03:01:22 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Logo from "../assets/logon.webp";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="h-fit w-full mt-[20vh] md:flex flex-col justify-center shadow-3xl  p-5">
+        <div>
+          <h1 className="font-semibold font-montserrat text-xl text-[#FEFBE5] text-left">
+            You can help <br /> Shape the Future
+          </h1>
+        </div>
+
+        <div className="mt-5 md:flex justify-between items-center">
+          <div className="w-[500px]">
+            <img src={Logo} alt="logo" className="w-full" />
+          </div>
+
+          <div>
+          <ul className="flex gap-2">
+            <li
+              className="font-semibold font-montserrat  hover:text-[#ff5757] cursor-pointer"
+              onClick={() => navigate("/")}
+            >
+              <a>Home |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat  hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("about").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>About |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("contact").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>Education |</a>
+            </li>
+            <li
+              className="font-semibold font-montserrat hover:text-[#ff5757] cursor-pointer"
+              onClick={()=>document.getElementById("contact").scrollIntoView({behavior:"smooth"})}
+            >
+              <a>Contact</a>
+            </li>
+          </ul>
+          </div>
+        </div>
+        <div className="flex  items-center justify-center mt-5">
+          <h1 className="flex justify-center text-xl text-left   font-montserrat font-semibold gap-2">
+            Created by<span className="font-bold text-xl  font-montserrat text-[#ff5757]">EcoLoc</span>team
+          </h1>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
